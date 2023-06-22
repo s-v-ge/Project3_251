9dbaa755e9d732d146cfc08f586175dedea825e9
Zach <118334392+s-v-ge@users.noreply.github.com>
2023-06-21 23:57:20 -0500

    Add files via upload

---
+++ tests.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include "barchartanimate.h"
+using namespace std;
+
+
+bool testBarDefaultConstructor() {
+	Bar b;
+    if (b.getName() != "") {
+    	cout << "testBarDefaultConstructor: getName failed" << endl;
+    	return false;
+    } else if (b.getValue() != 0) {
+    	cout << "testBarDefaultConstructor: getValue failed" << endl;
+    	return false;
+    } else if (b.getCategory() != "") {
+    	cout << "testBarDefaultConstructor: getCategory failed" << endl;
+    	return false;
+    }
+    cout << "testBarDefaultConstructor: all passed!" << endl;
+    return true;
+}
+
+bool testBarParamConstructor() {
+	Bar b("Chicago", 9234324, "US");
+    if (b.getName() != "Chicago") {
+    	cout << "testBarParamConstructor: getName failed" << endl;
+    	return false;
+    } else if (b.getValue() != 9234324) {
+    	cout << "testBarParamConstructor: getValue failed" << endl;
+    	return false;
+    } else if (b.getCategory() != "US") {
+    	cout << "testBarParamConstructor: getCategory failed" << endl;
+    	return false;
+    }
+    cout << "testBarParamConstructor: all passed!" << endl;
+    return true;
+}
+
+int main() {
+	testBarDefaultConstructor();
+	testBarParamConstructor();
+	
+    return 0;
+}
